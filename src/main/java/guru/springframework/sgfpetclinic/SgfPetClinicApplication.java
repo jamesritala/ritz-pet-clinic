ba6a5296152055a07ecf0cd26ad2c3d584f135af
James <jamesritala@gmail.com>
2018-11-16 15:27:48 +0200

    Created a new SpringBoot Application using Intellije Initializer

---
+++ src/main/java/guru/springframework/sgfpetclinic/SgfPetClinicApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sgfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgfPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgfPetClinicApplication.class, args);
+    }
+}
